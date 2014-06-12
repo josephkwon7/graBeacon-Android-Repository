5e85b4273a2e3bd5b169192b9ab064a10ae1fcc1
junhojohn <junhojohn@naver.com>
2014-06-12 20:31:06 +0900

    이미지URL요청 및 Bitmap Thread처리

--- TastyRoad02/src/com/example/wizturnbeacon/adapter/WizTurnBeaconListAdapter.java
+++ TastyRoad02/src/com/example/wizturnbeacon/adapter/WizTurnBeaconListAdapter.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -45,7 +46,10 @@ public class WizTurnBeaconListAdapter extends ArrayAdapter{
 	///Field	
 	private LayoutInflater inflater = null;
 	private ArrayList<WizTurnBeacons> wizTrunBeacon_items;
-
+	HashMap<WizTurnBeacons, Bitmap> getImgUrlMap = new HashMap<WizTurnBeacons, Bitmap>();
+	private String reqURL;
+	int count;
+	
 	///Constructor
 	public WizTurnBeaconListAdapter(Context context, int textViewResourceId,
 			ArrayList<WizTurnBeacons> mList) {
@@ -86,6 +90,14 @@ public class WizTurnBeaconListAdapter extends ArrayAdapter{
 	
 
 	public View getView(int position, View v, ViewGroup parent) {
+		
+		Bitmap thumbImg;
+		String req1 = "http://192.168.200.27:8080/tastyroad/beacon/Thumbnail/";
+		String req2 = wizTrunBeacon_items.get(position).getProximityUUID()+"/";
+		String req3 = wizTrunBeacon_items.get(position).getMajor()+"/";
+		String req4 = wizTrunBeacon_items.get(position).getMinor()+"";
+		reqURL = req1 + req2 + req3 + req4;
+		
 		if(v==null){
 			v = inflater.inflate(R.layout.array_scanlist, null);
 		}
@@ -93,88 +105,119 @@ public class WizTurnBeaconListAdapter extends ArrayAdapter{
 		TextView mSSID = (TextView)v.findViewById(R.id.scanList_SSID);
 		mSSID.setText("SSID: " + wizTrunBeacon_items.get(position).getName());
 		*/
-		
-		String req1 = "http://192.168.200.27:8080/tastyroad/beacon/Thumbnail/";
-		String req2 = wizTrunBeacon_items.get(position).getProximityUUID()+"/";
-		String req3 = wizTrunBeacon_items.get(position).getMajor()+"/";
-		String req4 = wizTrunBeacon_items.get(position).getMinor()+"";
-		String reqURL = req1 + req2 + req3 + req4;
-		Log.e(MainActivity.class.toString(), "RequestURL !!!!! : "+ reqURL);
-		
-		String rawJSON = getJSON(reqURL);
-		
-		Log.e(MainActivity.class.toString(), "RawJSON !!!!! : "+ rawJSON);
-
-		JSONParser parser = new JSONParser();
-		
-		
-		try {
-			Object obj = parser.parse(rawJSON);
-			JSONObject jsonObject = (JSONObject) obj;
-		 	String thumbURL = (String) jsonObject.get("imgSmall1");
-			Log.e(MainActivity.class.toString(), "URL !!!!! : "+thumbURL);
+		if(!getImgUrlMap.containsKey(wizTrunBeacon_items.get(position))){
+			RequestURLThread thread = new RequestURLThread(reqURL);
+			thread.start();
 			
-			URL url = new URL(thumbURL);
-			
-			Bitmap thumbImg = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-			//TextView mSSID = (TextView)v.findViewById(R.id.scanList_SSID);
-			//mSSID.setText("SSID: " + thumbURL);
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				
+				thumbImg = thread.getBitmap();
+				getImgUrlMap.put(wizTrunBeacon_items.get(position), thumbImg);
+		}
+		else{
+			thumbImg=getImgUrlMap.get(wizTrunBeacon_items.get(position));
+		}	
+		
 			ImageView thumbView = (ImageView)v.findViewById(R.id.imageView1);
 			thumbView.setImageBitmap(thumbImg);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	 	
 		
-		return v;
+			return v;
 		
 	}
 
-	public String getJSON(String address){
-		Log.e(MainActivity.class.toString(),"getJSON START!!!");
-
-		StringBuilder builder = new StringBuilder();
-		HttpClient client = new DefaultHttpClient();
-		HttpGet httpGet = new HttpGet(address);
-		try{
-			HttpResponse response = client.execute(httpGet);
-			StatusLine statusLine = response.getStatusLine();
-			int statusCode = statusLine.getStatusCode();
-			if(statusCode == 200){
-				Log.e(MainActivity.class.toString(),"getJSON OK!!!");
-				HttpEntity entity = response.getEntity();
-				InputStream content = entity.getContent();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-				String line;
-				while((line = reader.readLine()) != null){
-					builder.append(line);
-				}
-			} else {
-				Log.e(MainActivity.class.toString(),"Failed to get JSON object");
-			}
-		}catch(ClientProtocolException e){
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-		return builder.toString();
-	}
 		
 	//RSSI(전파수신강도)를 내림 차순으로 정렬. 즉 가까운 거리순으로 wiTurnBeacons 인스턴스 정렬
 	public void sort() {
 		Collections.sort(wizTrunBeacon_items, new Comparator<WizTurnBeacons>() {
 			@Override
 			public int compare(WizTurnBeacons lhs, WizTurnBeacons rhs) {
+				++count;
 				return (int) (rhs.getRssi() - lhs.getRssi());
 			}
 		});
+		
+		Log.e("Collection.sort ", "compare start ..." + count);
+	}
+	
+	public class RequestURLThread extends Thread{
+		
+		String reqURL;
+		Bitmap thumbImg;
+		
+		public RequestURLThread(){
+			
+		}
+		
+		public RequestURLThread(String reqURL) {
+			super();
+			this.reqURL = reqURL;
+		}
+		
+		public void run(){
+			Log.e(MainActivity.class.toString(), "RequestURL !!!!! : "+ reqURL);
+			Log.e(MainActivity.class.toString(),"getJSON START!!!");
+
+			StringBuilder builder = new StringBuilder();
+			HttpClient client = new DefaultHttpClient();
+			HttpGet httpGet = new HttpGet(reqURL);
+			try{
+				HttpResponse response = client.execute(httpGet);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				if(statusCode == 200){
+					Log.e(MainActivity.class.toString(),"getJSON OK!!!");
+					HttpEntity entity = response.getEntity();
+					InputStream content = entity.getContent();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+					String line;
+					while((line = reader.readLine()) != null){
+						builder.append(line);
+					}
+				} else {
+					Log.e(MainActivity.class.toString(),"Failed to get JSON object");
+				}
+			}catch(ClientProtocolException e){
+				e.printStackTrace();
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+			///////////////////////////////////////////////
+			
+			String rawJSON = builder.toString();
+			Log.e(MainActivity.class.toString(), "RawJSON !!!!! : "+ rawJSON);
+			JSONParser parser = new JSONParser();
+			
+			try {
+				Object obj = parser.parse(rawJSON);
+				JSONObject jsonObject = (JSONObject) obj;
+			 	reqURL = (String) jsonObject.get("imgSmall1");
+			 	Log.e(MainActivity.class.toString(), "URL !!!!! : "+reqURL);
+			 	
+				try {
+					URL url = new URL(reqURL);
+					thumbImg = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+					
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} catch (ParseException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		public Bitmap getBitmap(){
+			return thumbImg;
+		}
 	}
 	
 	
